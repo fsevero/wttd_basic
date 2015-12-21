1b480eee08b7b985a698e4ecfd2fe7e5d318f879
Severo <severo.fabricio@gmail.com>
2015-12-20 23:01:37 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Welcome to the Django
+=====================
+
+Template básico para quem deseja começar o desenvolvimento do seu site sem ter que configurar/instalar tudo do zero.
+
+# Instalação
+Baixe o projeto e rodar `vagrant up`.
+Na primeira vez que o comando for rodado, vários pacotes serão instalados, então o processo pode ser bastante lento.
+Nas próximas vezes, apenas a máquina virtual será carregada.
+
+# Conteúdo
+Sistema vem com git, pyenv com python 3.5.0, iPython notebook (com alias notebook), e com o virtualenv criado. Nenhuma configuração adicional foi criada.
---
+++ build/install.sh
@@ -0,0 +1,31 @@
+# Install essential packages from Apt
+sudo apt-get update -y
+
+#PyEnv requirements
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+
+#PyEnv install
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+#This is provisory. It will be in the .bash_aliases, but we need it know =D
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# Updating PyEnv
+pyenv update
+
+# Install Python 3.5.0 and set it to be the default python
+pyenv install 3.5.0
+pyenv global 3.5.0
+
+# iPython notebook
+pip install ipython[notebook]
+
+# Copy bash_aliases
+cp -p /vagrant_data/bash_aliases /home/vagrant/.bash_aliases
+
+pip install virtualenv
+mkdir /home/vagrant/shared/wttd
+cd /home/vagrant/shared/wttd
+virtualenv .wttd --always-copy -p `which python`
